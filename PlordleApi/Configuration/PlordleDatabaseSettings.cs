2add48a6a48427f04254294d0c1b43b6e76cb334
Darren <d.hislop17@gmail.com>
2022-06-20 18:35:16 -0400

    Simplified connection strings

--- PlordleApi/Configuration/PlordleDatabaseSettings.cs
+++ PlordleApi/Configuration/PlordleDatabaseSettings.cs
@@ -5,8 +5,4 @@ public class PlordleDatabaseSettings
     public string ConnectionString { get; set; } = null!;
     public string DatabaseName { get; set; } = null!;
     public string PlordleCollectionName { get; set; } = null!;
-    public string Host { get; set; } = null!;
-    public int Port { get; set; }
-    public string User { get; set; } = null!;
-    public string Password { get; set; } = null!;
 }
