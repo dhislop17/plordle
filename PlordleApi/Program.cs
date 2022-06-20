327a1af9dadb8091add6e1d6b431eb3e1ea4ad9d
dhislop17 <d.hislop17@gmail.com>
2022-06-20 01:08:14 -0400

    Initial dockerization work

--- PlordleApi/Program.cs
+++ PlordleApi/Program.cs
@@ -18,10 +18,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHealthChecks();
 
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- PlordleApi/Repositories/MongoPlordleDBContext.cs
+++ PlordleApi/Repositories/MongoPlordleDBContext.cs
@@ -7,7 +7,15 @@ public class MongoPlordleDBContext
 
     public MongoPlordleDBContext(IOptions<PlordleDatabaseSettings> options)
     {
-        _client = new MongoClient(options.Value.ConnectionString);
+        var host = options.Value.Host;
+        var port = options.Value.Port;
+        var user = options.Value.User;
+        var password = options.Value.Password;
+        var connString =  $@"mongodb+srv://{user}:{password}@{host}";
+
+        //var connString =  $@"mongodb://{host}:{port}";
+        //_client = new MongoClient(options.Value.ConnectionString);
+        _client = new MongoClient(connString);
         _database = _client.GetDatabase(options.Value.DatabaseName);
     }
 
