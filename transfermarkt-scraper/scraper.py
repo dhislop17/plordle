1a854ebfd2cd634bfa177ae126336c58112c4456
Darren <d.hislop17@gmail.com>
2024-08-07 12:18:28 -0700

    Fixing script to handle transfermarkt changes and some clean up

--- transfermarkt-scraper/scraper.py
+++ transfermarkt-scraper/scraper.py
@@ -1,86 +1,119 @@
 from lxml import html
 import requests
 import pandas as pd
-import numpy as np
 import re
 import time
 
-baseRoute = 'https://www.transfermarkt.us'
-leagueRoute = 'https://www.transfermarkt.us/premier-league/startseite/wettbewerb/GB1'
-headers = {'User-Agent': 
+BASE_ROUTE = 'https://www.transfermarkt.us'
+LEAGUE_ROUTE = 'https://www.transfermarkt.us/premier-league/startseite/wettbewerb/GB1'
+HEADERS = {'User-Agent': 
            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+NUMBER_OF_TEAMS = 20
+GOALKEEPER = "goalkeeper"
+DEFENDER = "defender"
+MIDFIELD = "midfielder"
+ATTACK = "attacker"
 
-page = requests.get(leagueRoute, headers= headers)
-leagueTree = html.fromstring(page.content)
+
+page = requests.get(LEAGUE_ROUTE, headers= HEADERS)
+league_tree = html.fromstring(page.content)
 start_time = time.time()
 
 teamMap = {}
 
-for i in range(20):
-    team = leagueTree.cssselect(".items tbody tr")[i].cssselect("td a")[0].attrib['title']
-    link = baseRoute + leagueTree.cssselect(".items tbody tr")[i].cssselect("td a")[0].attrib['href']
-    teamMap[team] = link
 
-teamMap
+for i in range(NUMBER_OF_TEAMS):
+    team = league_tree.cssselect(".items tbody tr")[i].cssselect("td a")[0].attrib['title']
+    link = BASE_ROUTE + league_tree.cssselect(".items tbody tr")[i].cssselect("td a")[0].attrib['href']
+    teamMap[team] = link
 
 names = []
 teams = []
 ages = []
-shirtNumbers = []
+shirt_numbers = []
 nationalities = []
 positions = []
-positionsTypes = []
+positions_types = []
 
-for k,v in teamMap.items():
-    page = requests.get(v, headers= headers)
+attacker_types = {"Centre-Forward", "Right Winger", "Left Winger"}
+midfield_types = {"Attacking Midfield", "Central Midfield", "Defensive Midfield"}
+defender_types = {"Right-Back", "Centre-Back", "Left-Back"}
+
+for team, teamUrl in teamMap.items():
+    page = requests.get(teamUrl, headers= HEADERS)
     teamTree = html.fromstring(page.content)
+
 
     for player in teamTree.cssselect('.items tbody tr.odd'):
         number = player.cssselect('td')[0].cssselect('td')[0].text_content()
-        name = player.cssselect('td td a')[1].attrib['title']
-        position = player.cssselect('td table tr')[1].cssselect('td')[0].text_content()
-        positionType = player.cssselect('td')[0].attrib['title'].lower()
-        age = re.search(r'\((\d{2})\)',player.cssselect('td')[6].text_content()).group(1)
+        name = player.cssselect('td td a')[0].text.strip()
+        position = player.cssselect('td table tr')[1].cssselect('td')[0].text.strip()
+
+        #Map position to position type to help with player guesses 
+        #Transfermarkt changed how this information is stored on the team page
+        if position in defender_types:
+            position_type = DEFENDER
+        elif position in midfield_types:
+            position_type = MIDFIELD
+        elif position in attacker_types:
+            position_type = ATTACK
+        else:
+            position_type = GOALKEEPER
+
+        ageElement = player.cssselect('td.zentriert')[1].text_content() 
+        strippedAge = re.search(r'\((\d{2})\)', ageElement).group(1)
         country = player.cssselect('td img.flaggenrahmen')[0].attrib['alt']
         
+        #Only Add Players who have assigned numbers to data frame, avoids suspended/loaned out/youth players
         if number != '-':
             names.append(name)
-            teams.append(k)
-            ages.append(int(age))
-            shirtNumbers.append(int(number))
+            teams.append(team)
+            ages.append(int(strippedAge))
+            shirt_numbers.append(int(number))
             nationalities.append(country)
             positions.append(position)
-            positionsTypes.append(positionType)
+            positions_types.append(position_type)
 
     for player in teamTree.cssselect('.items tbody tr.even'):
         number = player.cssselect('td')[0].cssselect('td')[0].text_content()
-        name = player.cssselect('td td a')[1].attrib['title']
-        position = player.cssselect('td table tr')[1].cssselect('td')[0].text_content()
-        positionType = player.cssselect('td')[0].attrib['title'].lower()
-        age = re.search(r'\((\d{2})\)',player.cssselect('td')[6].text_content()).group(1)
+        name = player.cssselect('td td a')[0].text.strip()
+        position = player.cssselect('td table tr')[1].cssselect('td')[0].text.strip()
+
+        #Map position to position type to help with player guesses 
+        #Transfermarkt changed how this information is stored on the team page
+        if position in defender_types:
+            position_type = DEFENDER
+        elif position in midfield_types:
+            position_type = MIDFIELD
+        elif position in attacker_types:
+            position_type = ATTACK
+        else:
+            position_type = GOALKEEPER
+
+        ageElement = player.cssselect('td.zentriert')[1].text_content() 
+        strippedAge = re.search(r'\((\d{2})\)', ageElement).group(1)
         country = player.cssselect('td img.flaggenrahmen')[0].attrib['alt']
 
         if number != '-':
             names.append(name)
-            teams.append(k)
-            ages.append(int(age))
-            shirtNumbers.append(int(number))
+            teams.append(team)
+            ages.append(int(strippedAge))
+            shirt_numbers.append(int(number))
             nationalities.append(country)
             positions.append(position)
-            positionsTypes.append(positionType)
+            positions_types.append(position_type)
 
 
-playerData = pd.DataFrame({
+player_data = pd.DataFrame({
     'Name': names,
     'Team': teams,
-    'PositionType': positionsTypes,
+    'PositionType': positions_types,
     'Position': positions,
-    'ShirtNumber' : shirtNumbers,
+    'ShirtNumber' : shirt_numbers,
     'Age': ages,
     'Country' : nationalities,
 })
 
-playerData = playerData.sort_values(by=['Team', 'ShirtNumber'])
-playerData.to_csv("PlayerData.csv", index_label="PlayerId")
+player_data = player_data.sort_values(by=['Team', 'ShirtNumber'])
+player_data.to_csv("PlayerData24.csv", index_label="PlayerId")
 print("--- %s seconds ---" % (time.time() - start_time))
-
