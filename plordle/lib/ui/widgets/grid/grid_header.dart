4d6334dbd7745e87a36e0a4b15f41c714ec2171b
Darren <d.hislop17@gmail.com>
2025-02-28 10:23:43 -0800

    switched to using a constant for the grid header's width

--- plordle/lib/ui/widgets/grid/grid_header.dart
+++ plordle/lib/ui/widgets/grid/grid_header.dart
@@ -8,32 +8,32 @@ class GridHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridNameHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridTeamHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridPositionHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridNumberHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridAgeHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
       Container(
-          width: 60,
+          width: Constants.gridHeaderWidth,
           alignment: Alignment.center,
           child: const Text(Constants.gridCountryHeader,
               maxLines: 1, overflow: TextOverflow.ellipsis)),
