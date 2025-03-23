4efa7b03aba92c40fea2170c091f705ed86fec45
Darren <d.hislop17@gmail.com>
2025-03-22 21:02:35 -0700

    Added a constants file for storage service keys and refactored storage service as part of game flow rework

--- plordle/lib/services/storage_service.dart
+++ plordle/lib/services/storage_service.dart
@@ -1,4 +1,6 @@
 import 'package:plordle/models/stat.dart';
+import 'package:plordle/models/user.dart';
+import 'package:plordle/ui/utils/storage_service_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageService {
@@ -9,56 +11,80 @@ class StorageService {
   }
 
   //UserViewModel Saved Data
-  Future<Stat> getUnlimitedModeStat() async {
-    int gamesPlayed = await prefs.getInt('unlimited_gp') ?? 0;
-    int wins = await prefs.getInt('unlimited_wins') ?? 0;
-    int losses = await prefs.getInt('unlimited_losses') ?? 0;
+  Future<User> getUser() async {
+    Stat normalModeStat = await _getNormalModeStat();
+    Stat challengeModeStat = await _getChallengeModeStat();
+    DateTime lastSolvedDate = await _getLastSolvedDate() ?? DateTime(2016);
+
+    return User(
+        normalModeStat: normalModeStat,
+        challengeModeStat: challengeModeStat,
+        lastAttemptedChallengeDate: lastSolvedDate);
+  }
+
+  Future<void> saveUser(User user) async {
+    await saveNormalModeStat(user.normalModeStat);
+    await saveMysteryModeStat(user.challengeModeStat);
+    await _saveLastSolvedDate(user.lastAttemptedChallengeDate);
+  }
+
+  Future<Stat> _getNormalModeStat() async {
+    int gamesPlayed =
+        await prefs.getInt(StorageServiceKeys.normalModeGamesPlayed) ?? 0;
+    int wins = await prefs.getInt(StorageServiceKeys.normalModeWins) ?? 0;
+    int losses = await prefs.getInt(StorageServiceKeys.normalModeLosses) ?? 0;
 
     return Stat(gamesPlayed: gamesPlayed, wins: wins, losses: losses);
   }
 
-  Future<void> saveUnlimitedModeStat(Stat stat) async {
-    await prefs.setInt('unlimited_gp', stat.gamesPlayed);
-    await prefs.setInt('unlimited_wins', stat.wins);
-    await prefs.setInt('unlimited_losses', stat.losses);
+  Future<void> saveNormalModeStat(Stat stat) async {
+    await prefs.setInt(
+        StorageServiceKeys.normalModeGamesPlayed, stat.gamesPlayed);
+    await prefs.setInt(StorageServiceKeys.normalModeWins, stat.wins);
+    await prefs.setInt(StorageServiceKeys.normalModeLosses, stat.losses);
   }
 
-  Future<Stat> getMysteryModeStat() async {
-    int gamesPlayed = await prefs.getInt('mystery_gp') ?? 0;
-    int wins = await prefs.getInt('mystery_wins') ?? 0;
-    int losses = await prefs.getInt('mystery_losses') ?? 0;
+  Future<Stat> _getChallengeModeStat() async {
+    int gamesPlayed =
+        await prefs.getInt(StorageServiceKeys.challengeModeGamesPlayed) ?? 0;
+    int wins = await prefs.getInt(StorageServiceKeys.challengeModeWins) ?? 0;
+    int losses =
+        await prefs.getInt(StorageServiceKeys.challengeModeLosses) ?? 0;
 
     return Stat(gamesPlayed: gamesPlayed, wins: wins, losses: losses);
   }
 
   Future<void> saveMysteryModeStat(Stat stat) async {
-    await prefs.setInt('mystery_gp', stat.gamesPlayed);
-    await prefs.setInt('mystery_wins', stat.wins);
-    await prefs.setInt('mystery_losses', stat.losses);
-  }
-
-  Future<bool> getSolvedMystery() async {
-    return await prefs.getBool('solved_mystery') ?? false;
-  }
-
-  Future<void> saveSolvedMystery(bool solvedMystery) async {
-    await prefs.setBool('solved_mystery', solvedMystery);
+    await prefs.setInt(
+        StorageServiceKeys.challengeModeGamesPlayed, stat.gamesPlayed);
+    await prefs.setInt(StorageServiceKeys.challengeModeWins, stat.wins);
+    await prefs.setInt(StorageServiceKeys.challengeModeLosses, stat.losses);
   }
 
   Future<bool> getOnboardingStatus() async {
-    return await prefs.getBool('onboarding_done') ?? false;
+    return await prefs.getBool(StorageServiceKeys.onboardingDone) ?? false;
   }
 
   Future<void> saveOnboardingStatus(bool onboardingFinished) async {
-    await prefs.setBool('onboarding_done', onboardingFinished);
+    await prefs.setBool(StorageServiceKeys.onboardingDone, onboardingFinished);
   }
 
   Future<String> getDifficulty() async {
-    return await prefs.getString('difficulty') ?? 'easy';
+    return await prefs.getString(StorageServiceKeys.difficulty) ?? 'easy';
   }
 
   Future<void> saveDifficulty(String difficulty) async {
-    await prefs.setString('difficulty', difficulty);
+    await prefs.setString(StorageServiceKeys.difficulty, difficulty);
+  }
+
+  Future<DateTime?> _getLastSolvedDate() async {
+    String lastDate =
+        await prefs.getString(StorageServiceKeys.lastSolvedDate) ?? 'none';
+    return DateTime.tryParse(lastDate);
+  }
+
+  Future<void> _saveLastSolvedDate(DateTime date) async {
+    await prefs.setString(StorageServiceKeys.lastSolvedDate, date.toString());
   }
 
   //ThemeViewModel Saved Data
@@ -66,46 +92,48 @@ class StorageService {
   /// Gets the name of the theme saved in data or defaults
   /// to the Premier League theme
   Future<String> getThemeName() async {
-    return await prefs.getString('theme_name') ?? 'Premier League';
+    return await prefs.getString(StorageServiceKeys.themeName) ??
+        'Premier League';
   }
 
   /// Saves the name of the theme in use
   Future<void> saveThemeName(String themeName) async {
-    await prefs.setString('theme_name', themeName);
+    await prefs.setString(StorageServiceKeys.themeName, themeName);
   }
 
   //PlayerViewModel Saved Data
   Future<List<String>> getExcludedTeams() async {
-    return await prefs.getStringList('excluded_teams') ?? [];
+    return await prefs.getStringList(StorageServiceKeys.excludedTeams) ?? [];
   }
 
   Future<void> saveExcludedTeams(List<String> excludedTeams) async {
-    await prefs.setStringList('excluded_teams', excludedTeams);
+    await prefs.setStringList(StorageServiceKeys.excludedTeams, excludedTeams);
   }
 
   //Saved Data Clear Functions
 
   void clearThemeModelData() async {
-    await prefs.remove('theme_name');
+    await prefs.remove(StorageServiceKeys.themeName);
   }
 
   void clearFilterListData() async {
-    await prefs.remove('excluded_teams');
+    await prefs.remove(StorageServiceKeys.excludedTeams);
   }
 
   void clearUserModelData() async {
-    await prefs.remove('onboarding_done');
-    await prefs.remove('solved_mystery');
+    await prefs.remove(StorageServiceKeys.onboardingDone);
+    await prefs.remove(StorageServiceKeys.difficulty);
+    await prefs.remove(StorageServiceKeys.lastSolvedDate);
     resetStats();
   }
 
   void resetStats() async {
-    await prefs.remove('unlimited_gp');
-    await prefs.remove('unlimited_wins');
-    await prefs.remove('unlimited_losses');
-    await prefs.remove('mystery_gp');
-    await prefs.remove('mystery_wins');
-    await prefs.remove('mystery_losses');
+    await prefs.remove(StorageServiceKeys.normalModeGamesPlayed);
+    await prefs.remove(StorageServiceKeys.normalModeWins);
+    await prefs.remove(StorageServiceKeys.normalModeLosses);
+    await prefs.remove(StorageServiceKeys.challengeModeGamesPlayed);
+    await prefs.remove(StorageServiceKeys.challengeModeWins);
+    await prefs.remove(StorageServiceKeys.challengeModeLosses);
   }
 
   //TODO: implement handling a game that is in progess when the app closes
---
+++ plordle/lib/ui/utils/storage_service_keys.dart
@@ -0,0 +1,17 @@
+class StorageServiceKeys {
+  static const String normalModeGamesPlayed = "normal_gp";
+  static const String normalModeWins = "normal_wins";
+  static const String normalModeLosses = "normal_losses";
+
+  static const String challengeModeGamesPlayed = "challenge_gp";
+  static const String challengeModeWins = "challenge_wins";
+  static const String challengeModeLosses = "challenge_losses";
+
+  static const String onboardingDone = "onboarding_done";
+  static const String difficulty = "difficulty";
+  static const String lastSolvedDate = "last_solved_date";
+
+  static const String themeName = "theme_name";
+
+  static const String excludedTeams = "excluded_teams";
+}
