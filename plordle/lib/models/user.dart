0aa320097ce371074b65c91fef4c3f880acdaf51
Darren <d.hislop17@gmail.com>
2025-03-22 21:03:12 -0700

    Moved stat tracking functionality to the user class

--- plordle/lib/models/user.dart
+++ plordle/lib/models/user.dart
@@ -1,12 +1,47 @@
+import 'package:plordle/models/stat.dart';
+
 class User {
-  int wins;
-  int losses;
-  bool isInUnlimitedMode;
-  int numOfGuesses;
+  Stat normalModeStat;
+  Stat challengeModeStat;
+  DateTime lastAttemptedChallengeDate;
+
+  String get normalModeStatString => normalModeStat.toString();
+
+  String get challengeModeStatString => challengeModeStat.toString();
 
   User(
-      {required this.wins,
-      required this.losses,
-      required this.isInUnlimitedMode,
-      required this.numOfGuesses});
+      {required this.normalModeStat,
+      required this.challengeModeStat,
+      required this.lastAttemptedChallengeDate});
+
+  void playedNormalModeGame(bool wonGame) {
+    normalModeStat.gamesPlayed++;
+    if (wonGame) {
+      normalModeStat.wins++;
+    } else {
+      normalModeStat.losses++;
+    }
+  }
+
+  void playedChallengeModeGame(bool wonGame) {
+    challengeModeStat.gamesPlayed++;
+    var today = DateTime.now();
+    lastAttemptedChallengeDate = DateTime(today.year, today.month, today.day);
+
+    if (wonGame) {
+      challengeModeStat.wins++;
+    } else {
+      challengeModeStat.losses++;
+    }
+  }
+
+  void resetUser() {
+    resetStats();
+    lastAttemptedChallengeDate = DateTime(2016);
+  }
+
+  void resetStats() {
+    normalModeStat = Stat(gamesPlayed: 0, wins: 0, losses: 0);
+    challengeModeStat = Stat(gamesPlayed: 0, wins: 0, losses: 0);
+  }
 }
