cacbef4d3148c03c8d03bea42473523cbcf36dbc
Darren <d.hislop17@gmail.com>
2025-03-24 18:19:36 -0700

    Removing the API

--- PlordleApi/Configuration/PlordleDatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PlordleApi.Configuration;
-
-public class PlordleDatabaseSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string PlordleCollectionName { get; set; } = null!;
-}
--- PlordleApi/Repositories/using.cs
+++
@@ -1,1 +0,0 @@
-﻿global using MongoDB.Driver;
