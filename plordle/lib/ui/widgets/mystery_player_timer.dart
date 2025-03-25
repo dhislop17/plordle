c9eeae153e1b801c9c202abc54fa666038a42a59
Darren <d.hislop17@gmail.com>
2025-03-24 18:04:58 -0700

    Updated timer styling

--- plordle/lib/ui/widgets/mystery_player_timer.dart
+++ plordle/lib/ui/widgets/mystery_player_timer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
-import 'package:plordle/ui/utils/app_theme.dart';
+import 'package:plordle/view_models/theme_view_model.dart';
 import 'package:plordle/view_models/user_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -15,35 +15,37 @@ class MysteryPlayerTimer extends StatelessWidget {
         .add(const Duration(days: 1));
 
     var model = Provider.of<UserViewModel>(context, listen: false);
-    var rowChildren = [
-      _buildTimerContent(),
-      TimerCountdown(
-        format: CountDownTimerFormat.hoursMinutesSeconds,
-        endTime: nextTime,
-        onEnd: () async {
-          Navigator.pop(context);
-          model.getNextChallengeModePlayer();
-        },
-        timeTextStyle: const TextStyle(
-            color: Themes.premGreen, fontWeight: FontWeight.bold, fontSize: 20),
-        spacerWidth: 2,
-        enableDescriptions: false,
-      )
-    ];
-
-    //Return the row as is, if the challenge has been completed for that day
-    if (completedChallenge) {
-      return Row(children: rowChildren);
-    } else {
-      //reverse the list so that the text comes after the timer
-      return Row(children: rowChildren.reversed.toList());
-    }
+    var themeModel = Provider.of<ThemeViewModel>(context, listen: false);
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        _buildTimerContent(),
+        TimerCountdown(
+          format: CountDownTimerFormat.hoursMinutesSeconds,
+          endTime: nextTime,
+          onEnd: () async {
+            Navigator.pop(context);
+            model.getNextChallengeModePlayer();
+          },
+          timeTextStyle: TextStyle(
+              color: themeModel.getDecorationColor(isDarkMode),
+              fontWeight: FontWeight.bold,
+              fontSize: 24),
+          spacerWidth: 2,
+          hoursDescription: "hr",
+          minutesDescription: "m",
+          secondsDescription: "s",
+        ),
+      ],
+    );
   }
 
   Widget _buildTimerContent() {
     Text timerText = (completedChallenge)
-        ? Text("New challenge mode player in: ")
-        : Text(" remaining to complete today's challenge.");
+        ? Text("New daily challenge in:")
+        : Text("Time left for today's challenge:");
 
     return timerText;
   }
